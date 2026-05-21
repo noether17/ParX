9ce320189580399ef6975e185f59f2986ccbe405
Jonathan White <jawh@umich.edu>
2026-05-21 10:52:31 -0600

    Replaced 'doer' with 'executor' in tests.

--- test/ThreadPoolExecutorTest.cpp
+++ test/ThreadPoolExecutorTest.cpp
@@ -18,9 +18,9 @@ TEST(ThreadPoolExecutorTest, MultiplyTest) {
   }();
   auto const b = a;
   auto c = std::vector<double>(N);
-  auto doer = ParX::ThreadPoolExecutor(n_threads);
+  auto executor = ParX::ThreadPoolExecutor(n_threads);
 
-  call_kernel<multiply_kernel>(doer, N, a.data(), b.data(), c.data());
+  call_kernel<multiply_kernel>(executor, N, a.data(), b.data(), c.data());
 
   for (auto i = 0; i < N; ++i) {
     EXPECT_DOUBLE_EQ(a[i] * b[i], c[i]);
