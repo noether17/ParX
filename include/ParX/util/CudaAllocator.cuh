16f7dbb0973bb2cf20dfb86368026a8aa436bbbb
Jonathan White <jawh@umich.edu>
2026-07-27 21:31:06 -0600

    Removed redundant CudaPtr<T>::unwrap_device_ptr() member function.

--- include/ParX/util/CudaAllocator.cuh
+++ include/ParX/util/CudaAllocator.cuh
@@ -28,9 +28,8 @@ class CudaPtr {
 
   template <CudaElementType U>
   constexpr CudaPtr(CudaPtr<U> other) noexcept
-      : dev_ptr_{reinterpret_cast<pointer>(other.unwrap_device_ptr())} {}
+      : dev_ptr_{reinterpret_cast<pointer>(other.unwrap())} {}
 
-  constexpr auto unwrap_device_ptr() const noexcept { return dev_ptr_; }
   constexpr auto unwrap() const noexcept { return dev_ptr_; }
   static constexpr auto wrap_device_ptr(pointer dev_ptr) noexcept {
     return CudaPtr{dev_ptr};
@@ -71,30 +70,29 @@ struct CudaAllocator {
     return device_pointer::wrap_device_ptr(raw_dev_ptr);
   }
   static void deallocate(device_pointer dev_ptr) noexcept {
-    CUDA_ERROR_CHECK(cudaFree(dev_ptr.unwrap_device_ptr()));
+    CUDA_ERROR_CHECK(cudaFree(dev_ptr.unwrap()));
   }
 
   // initialization and data transfer
   static void set_n_zero(device_pointer dev_ptr, size_type n) noexcept {
-    CUDA_ERROR_CHECK(cudaMemset(dev_ptr.unwrap_device_ptr(), 0, n * sizeof(T)));
+    CUDA_ERROR_CHECK(cudaMemset(dev_ptr.unwrap(), 0, n * sizeof(T)));
     CUDA_ERROR_CHECK(cudaDeviceSynchronize());
   }
   static void copy_n_from_host(device_pointer dev_ptr, const_pointer host_ptr,
                                size_type n) noexcept {
-    CUDA_ERROR_CHECK(cudaMemcpy(dev_ptr.unwrap_device_ptr(), host_ptr,
-                                n * sizeof(T), cudaMemcpyHostToDevice));
+    CUDA_ERROR_CHECK(cudaMemcpy(dev_ptr.unwrap(), host_ptr, n * sizeof(T),
+                                cudaMemcpyHostToDevice));
   }
   static void copy_n_from_device(device_pointer dev_ptr,
                                  const_device_pointer source_ptr,
                                  size_type n) noexcept {
-    CUDA_ERROR_CHECK(cudaMemcpy(dev_ptr.unwrap_device_ptr(),
-                                source_ptr.unwrap_device_ptr(), n * sizeof(T),
-                                cudaMemcpyDeviceToDevice));
+    CUDA_ERROR_CHECK(cudaMemcpy(dev_ptr.unwrap(), source_ptr.unwrap(),
+                                n * sizeof(T), cudaMemcpyDeviceToDevice));
   }
   static void copy_n_to_host(pointer host_ptr, const_device_pointer dev_ptr,
                              size_type n) noexcept {
-    CUDA_ERROR_CHECK(cudaMemcpy(host_ptr, dev_ptr.unwrap_device_ptr(),
-                                n * sizeof(T), cudaMemcpyDeviceToHost));
+    CUDA_ERROR_CHECK(cudaMemcpy(host_ptr, dev_ptr.unwrap(), n * sizeof(T),
+                                cudaMemcpyDeviceToHost));
   }
 };
 }  // namespace ParX
